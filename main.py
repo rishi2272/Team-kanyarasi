a62940124516af1e1a29759650b1a697d6a64597
rishi2272 <23jr1a05h7@gmail.com>
2025-08-27 16:19:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ def generateAI():
     y=dataset.iloc[:,-1].values
 
     from sklearn.model_selection import train_test_split
-    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2)
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
 
     from sklearn.neighbors import KNeighborsClassifier
     ai = KNeighborsClassifier(n_neighbors=5)
 
-    ai.fit(x_train,y_train)
+    ai.fit(X_train,y_train)
 
     import pickle
     pickle.dump(ai,open('model.pkl','wb'))
