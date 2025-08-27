231ce696a39a87056ceb8e62460df956fb63fa5f
rishi2272 <23jr1a05h7@gmail.com>
2025-08-27 15:46:44 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def generateAI():
+    dataset = pd.read_csv('data.csv')
+    dataset = dataset.dropna()
+    X= dataset.iloc[:,1].values
+    X= X.reshape(-1,1)
+    y=dataset.iloc[:,-1].values
+
+    from sklearn.model_selection import train_test_split
+    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2)
+
+    from sklearn.neighbors import KNeighborsClassifier
+    ai = KNeighborsClassifier(n_neighbors=5)
+
+    ai.fit(x_train,y_train)
+
+    import pickle
+    pickle.dump(ai,open('model.pkl','wb'))
